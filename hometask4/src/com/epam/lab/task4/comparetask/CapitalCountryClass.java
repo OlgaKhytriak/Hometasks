38b0868d7864bb272c538bee17a04ce904170330
OlgaKhytriak <KhytriakOlga@gmail.com>
2017-11-05 17:41:18 +0200

    Compare task begin

---
+++ hometask4/src/com/epam/lab/task4/comparetask/CapitalCountryClass.java
@@ -0,0 +1,31 @@
+package com.epam.lab.task4.comparetask;
+
+public class CapitalCountryClass {
+	private String capital;
+	private String country;
+
+	public CapitalCountryClass() {
+	}
+
+	public CapitalCountryClass(String capital, String country) {
+		this.capital = capital;
+		this.country = country;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ hometask4/src/com/epam/lab/task4/comparetask/StartTask4Nom5.java
@@ -0,0 +1,10 @@
+package com.epam.lab.task4.comparetask;
+
+public class StartTask4Nom5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
