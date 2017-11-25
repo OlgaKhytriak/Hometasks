0ccaa5afb8c32d657b73a185d21068aa37c7c67c
OlgaKhytriak <KhytriakOlga@gmail.com>
2017-11-25 15:17:49 +0200

    Student class

--- hometask11/dbframework/src/main/java/com/epam/dbframework/App.java
+++ hometask11/dbframework/src/main/java/com/epam/dbframework/App.java
@@ -5,43 +5,34 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import org.apache.log4j.Logger;
 
+import com.epam.dbframework.handler.ConnectorToDB;
+import com.epam.dbframework.model.elements.Student;
 
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	 private static final Logger LOG = Logger.getLogger(App.class);
     public static void main( String[] args ) throws SQLException
     {
     	LOG.info( "Hello World!" );
-       
-        
-        try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			LOG.info("Driver not found error");
-		}  
-    	String jdbcUrl = "jdbc:mysql://localhost:3306/sample";
-    	String user     = "root";
-    	String password = "123qaz";
-    	Connection connection = DriverManager.getConnection(jdbcUrl,user,password);
-    	
-    	Statement stmt = connection.createStatement();
+    	Student st=new Student(12, "Olga", "Petrova", new Date(1), "Lviv", "phone", 12, 14);
+    		System.out.println(st.toString());	
+    	Connection connection=new ConnectorToDB().connect();
+    	Statement statement = connection.createStatement();
     	String sql = "SELECT * FROM department";
-    	ResultSet resultSet = stmt.executeQuery(sql);
+    	ResultSet resultSet = statement.executeQuery(sql);
     	while(resultSet.next()){
     		String number = resultSet.getString("dept_no");
     		String name = resultSet.getString("dept_name");
     		String location = resultSet.getString("location");
     		LOG.info(number+"  " +name+ "  "+location);
     	}
-
+    	resultSet.close();
+    	statement.close();
+    	connection.close();
 
     }
 }
