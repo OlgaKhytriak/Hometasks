1574d7d36e0a0917dbf3b3f639385a4fc25964af
OlgaKhytriak <KhytriakOlga@gmail.com>
2017-11-30 16:41:29 +0200

    Entiy and FieldAnn are added

---
+++ hometaskBigProject/src/main/java/com/epam/finalproject/anotations/FieldAnn.java
@@ -0,0 +1,13 @@
+package com.epam.finalproject.anotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldAnn {
+	String value() default "null";
+	String type() default "VARCHAR(45)";
+}
