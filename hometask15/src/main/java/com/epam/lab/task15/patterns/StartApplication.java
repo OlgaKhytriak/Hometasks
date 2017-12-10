94a42b58c80121d45e6cff01c72f5417ef994202
OlgaKhytriak <KhytriakOlga@gmail.com>
2017-12-10 10:30:23 +0200

    Main pacages are created

---
+++ hometask15/src/main/java/com/epam/lab/task15/patterns/StartApplication.java
@@ -0,0 +1,39 @@
+package com.epam.lab.task15.patterns;
+
+import java.io.IOException;
+
+import com.epam.lab.task15.patterns.examples.*;
+
+public class StartApplication {
+	public static void main(String[] args) throws Exception {
+		useBridgePatterns();
+		useDecoratorPatterns();
+		useFacadePatterns();
+		useStrategyPatterns();
+		
+	}
+
+	private static void useBridgePatterns() {
+		BridgeExample builderExamples = new BridgeExample();
+		System.out.println("---------- Bridge Example ------------------------------------");
+		
+	}
+	private static void useDecoratorPatterns() {
+		DecoratorExample decoratorExample = new DecoratorExample();
+		System.out.println("---------- Decorator Example ------------------------------------");
+		
+	}
+	private static void useFacadePatterns() {
+		FacadeExample facadeExample = new FacadeExample();
+		System.out.println("---------- Facade Example ------------------------------------");
+		
+	}
+	private static void useStrategyPatterns() {
+		StrategyExample strategyExample = new StrategyExample();
+		System.out.println("---------- Strategy Example ------------------------------------");
+		
+	}
+
+
+
+}
---
+++ hometask15/src/main/java/com/epam/lab/task15/patterns/examples/StrategyExample.java
@@ -0,0 +1,5 @@
+package com.epam.lab.task15.patterns.examples;
+
+public class StrategyExample {
+
+}
