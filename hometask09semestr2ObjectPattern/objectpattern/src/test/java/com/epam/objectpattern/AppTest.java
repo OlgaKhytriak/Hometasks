141d45afd721b2f26bb42d76f8d9853bd798d50b
OlgaKhytriak <KhytriakOlga@gmail.com>
2018-02-01 14:09:56 +0200

    Old xpatheS were deleted

--- hometask09semestr2ObjectPattern/objectpattern/src/test/java/com/epam/objectpattern/AppTest.java
+++ hometask09semestr2ObjectPattern/objectpattern/src/test/java/com/epam/objectpattern/AppTest.java
@@ -48,25 +48,21 @@ public class AppTest extends TestCase {
 	@Test
 	public void testApp() {
 		driver.findElement(By.xpath("//input[@type='email'][@name='identifier']")).sendKeys(MY_LOGIN);
-		//driver.findElement(By.xpath("//div[@role='button']//span[contains(text(),'Next')]")).click();
-		 driver.findElement(By.id("identifierNext")).click();
+				 driver.findElement(By.id("identifierNext")).click();
 		WebElement myElement = driver.findElement(By.xpath("//input[@type='password'][@name='password']"));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("elem=arguments[0]; elem.setAttribute('type','text'); ", myElement);
 		myElement.sendKeys(MY_PASSWORD);
 		driver.findElement(By.id("passwordNext")).click();
-		//driver.findElement(By.xpath("//div[@role='button'][contains(text(),'COMPOSE')]")).click();
-		//driver.findElement(By.xpath("//div[@class='T-I J-J5-Ji T-I-KE L3'][@role='button']")).click();
+		
 		driver.findElement(By.xpath("//div[@role='navigation']/preceding-sibling::div//div[@role='button']")).click();
 		
-		//WebElement elementToSent = driver
-		//		.findElement(By.xpath("//form[@enctype='multipart/form-data']/div[1]/table[1]//textarea"));
+		
 		WebElement elementToSent = driver
 				.findElement(By.xpath("//form[@enctype='multipart/form-data']//textarea[@name='to']"));	
 		elementToSent.sendKeys(SENT_TO);
 		elementToSent.click();
-		//driver.findElement(By.xpath("//div[@aria-label='Message Body']")).click();
-		//driver.findElement(By.xpath("//div[@role='textbox']")).click();
+		
 		driver.findElement(By.xpath("//form[@enctype='multipart/form-data']/following::table//div[@role='textbox']")).click();
 		
 		WebElement elementSubject = driver
@@ -74,12 +70,10 @@ public class AppTest extends TestCase {
 		Actions builder = new Actions(driver);
 		builder.moveToElement(elementSubject).build().perform();
 		elementSubject.sendKeys("Test");
-		//WebElement elementMessage = driver.findElement(By.xpath("//div[@aria-label='Message Body']"));
-		//WebElement elementMessage = driver.findElement(By.xpath("//div[@role='textbox']"));
+		
 		WebElement elementMessage = driver.findElement(By.xpath("//form[@enctype='multipart/form-data']/following::table//div[@role='textbox']"));
 		builder.moveToElement(elementMessage).click().sendKeys(MESSAGE_TEXT).perform();
-		//WebElement elementCloser = driver.findElement(By.xpath("//img[@aria-label='Save & Close']"));
-		//elementCloser.click();
+		
 		elementMessage.sendKeys(Keys.ESCAPE);
 		driver.get("https://mail.google.com/mail/u/0/#drafts");
 		
@@ -92,23 +86,17 @@ public class AppTest extends TestCase {
 				.findElement(By.xpath("//div[@role='link'][contains(.,'"+String.format("%s", MESSAGE_TEXT)+"')]"));
 		builder.moveToElement(elementLinkDiv).click().perform();
 		
-		//WebElement elementSentButton = driver.findElement(By.xpath(
-		//		"//form[@enctype='multipart/form-data']/following-sibling::table//div[@role='button'][contains(@class, 'J-J5-Ji')]"));
-		//elementSentButton.click();
-		//WebElement elementSentForm = driver.findElement(By.xpath("//div[@role='textbox'][contains(@class, 'editable')] "));
+		
 		WebElement elementSentForm = driver.findElement(By.xpath("//form[@enctype='multipart/form-data']/following::table//div[@role='textbox']"));
 		WebElement elementSentFormButton = driver.findElement(By.xpath("//form[@enctype='multipart/form-data']/following::table/tbody/child::tr[2]/descendant::table//div[@role='button'][1]"));
 		elementSentFormButton.click();
-		//++
-		//String keysPressed =  Keys.chord(Keys.CONTROL, Keys.ENTER);
-		//elementSentForm.sendKeys(keysPressed) ;
-		//++
+		
 		driver.get("https://mail.google.com/mail/u/0/#sent");
-		//driver.findElement(By.xpath("//a[@href='https://mail.google.com/mail/#sent']")).click();
+		
 		List<WebElement> elementsDrafts = driver
 				.findElements(By.xpath("//div[contains(text(),'"+String.format("%s", MESSAGE_TEXT)+"')]"));
 		assertTrue("Draft has been sended",(elementsDrafts.size()> 0));
-		System.out.println("1111111111111111");
+		
 	}
 	
 	@AfterClass
@@ -116,7 +104,7 @@ public class AppTest extends TestCase {
         if (driver != null) {
           // driver.close();
           //  driver.quit();
-//    		"//form[@enctype='multipart/form-data']/following::table//div[@role='textbox']"));
+
 }
 	 }
 
