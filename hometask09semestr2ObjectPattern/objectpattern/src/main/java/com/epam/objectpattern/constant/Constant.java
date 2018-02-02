b89dca35af09cb09ed75c73dc91125aa1316b0db
OlgaKhytriak <KhytriakOlga@gmail.com>
2018-02-02 14:57:19 +0200

    Finish

--- hometask09semestr2ObjectPattern/objectpattern/src/main/java/com/epam/objectpattern/constant/Constant.java
+++ hometask09semestr2ObjectPattern/objectpattern/src/main/java/com/epam/objectpattern/constant/Constant.java
@@ -7,6 +7,9 @@ public class Constant {
 	public static final String CHROME_DRIVER_TYPE = "webdriver.chrome.driver";
 	public static final String CHROME_DRIVER_PATH = "src/main/resources/chromedriver.exe";
 	
+	public static final String FIREFOX_DRIVER_TYPE = "webdriver.chrome.driver";  // !!! cHANGE
+	public static final String FIREFOX_DRIVER_PATH = "src/main/resources/chromedriver.exe"; // !!! cHANGE
+	
 	
 	public static final String WEB_SITE_URL = "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
 	
--- hometask09semestr2ObjectPattern/objectpattern/src/main/java/com/epam/objectpattern/driverfactory/FirefoxDriverManager.java
+++ hometask09semestr2ObjectPattern/objectpattern/src/main/java/com/epam/objectpattern/driverfactory/FirefoxDriverManager.java
@@ -1,20 +1,19 @@
 package com.epam.objectpattern.driverfactory;
 
-import static com.epam.objectpattern.constant.Constant.CHROME_DRIVER_PATH;
-import static com.epam.objectpattern.constant.Constant.CHROME_DRIVER_TYPE;
+import static com.epam.objectpattern.constant.Constant.FIREFOX_DRIVER_PATH;
+import static com.epam.objectpattern.constant.Constant.FIREFOX_DRIVER_TYPE;
 import static com.epam.objectpattern.constant.Constant.GLOBAL_WAIT_TIME;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirefoxDriverManager extends DriverManager{
-	
-	
+public class FirefoxDriverManager extends DriverManager {
+
 	@Override
 	protected void createDriver() {
-		System.setProperty(CHROME_DRIVER_TYPE, CHROME_DRIVER_PATH); //!!!
-		driver = new ChromeDriver();//!!!
+		System.setProperty(FIREFOX_DRIVER_TYPE, FIREFOX_DRIVER_PATH); // !!!
+		driver = new ChromeDriver();// !!!
 		driver.manage().timeouts().implicitlyWait(GLOBAL_WAIT_TIME, TimeUnit.SECONDS);
 
 	}
